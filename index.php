4a253438449935c1bb48c4d7b3c395879250ae14
Hilaludin Wahid <35256757+hylal@users.noreply.github.com>
2019-07-18 20:07:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,14 +14,17 @@ if($method == 'POST'){
 			$speech = "Hi, Nice to meet you";
 			break;
 
-		case 'bye':
+		case 'bye'
 			$speech = "Bye, good night";
 			break;
 
-		case 'anything':
+		case 'anything'
 			$speech = "Yes, you can type anything here.";
 			break;
-
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
 	}
 
 	$response = new \stdClass();
